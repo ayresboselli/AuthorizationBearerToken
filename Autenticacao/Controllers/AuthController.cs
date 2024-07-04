4f21a2d5407d5c3d6758b7cd984dcc9dabd5d0ab
unknown <ayres.piovesan@xkeysti.com.br>
2024-07-04 05:52:08 -0300

    Inicial

---
+++ Autenticacao/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Autenticacao.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("/login")]
+        public IActionResult Login(string username, string password)
+        {
+            if(IsValidUser(username, password))
+            {
+                var claimsPrincipal = new ClaimsPrincipal(
+                    new ClaimsIdentity(
+                        new [] { 
+                            new Claim(ClaimTypes.Name, username),
+                            new Claim(ClaimTypes.Role, "admin"),
+                            new Claim(ClaimTypes.Role, "manager"),
+                        },
+                        BearerTokenDefaults.AuthenticationScheme
+                    )
+                );
+
+                return SignIn(claimsPrincipal);
+            }
+
+            return Unauthorized();
+        }
+
+        private bool IsValidUser(string username, string password) => username == "user" && password == "1234";
+
+        [HttpGet("/user")]
+        [Authorize(Roles = "admin")]
+        public IActionResult GetUser()
+        {
+            var user = User;
+            if (user?.Identity?.IsAuthenticated ?? false)
+            {
+                return Ok();
+            }
+
+            return Unauthorized();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Bearer Token Authentication in .net *
+
+To log in and generate the token, use the [Login](http://localhost:5229/login?username=user&password=1234 "Login") URL.
+With the bearer token added in Authorization header, use the [User](http://localhost:5229/user "User") URL to test this token
